3a6f681aa124dddb7635bc5983926f27dd9bbdd4
NTDM <n.toledo22@gmail.com>
2016-11-05 13:43:16 -0200

    Implementado a movimentação do boneco

--- src/Game.java
+++ src/Game.java
@@ -8,17 +8,19 @@ import javax.swing.*;
 public class Game extends JFrame{
 	
 	Screen s1;
+	TelaGamePlay tg1;
 	
 	public Game(){
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setTitle("Collect the Garbage");
-		this.setBackground(new Color(0, 77, 0));
 		this.setBounds(10,10,905,700);
 		this.setResizable(false);
 		this.setVisible(true);
 		
 		s1 = new Screen();
+		tg1 = new TelaGamePlay();
 		
+		this.add(tg1);
 		this.add(s1);
 	}
 	
--- src/Screen.java
+++ src/Screen.java
@@ -1,32 +1,19 @@
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
 import javax.swing.*;
 
 public class Screen extends JPanel{
 	
-	private ImageIcon titulo;
-	
-	public Screen(){
-		
-	}
-	
 	public void paint(Graphics g){
 		//desenha a borda
-		g.setColor(Color.WHITE);
+		g.setColor(Color.RED);
 		g.drawRect(24, 10, 851, 55);
-		
-		//desenha o titulo da imagem
-		titulo = new ImageIcon("alguma-imagem-bolada-pra-quem-manja-de-photoshop-kkkkk.jpg");
-		titulo.paintIcon(this, g, 25, 11);
-		
-		//desenha a borda da tela de gameplay
-		g.setColor(Color.white);
-		g.drawRect(24, 74, 851, 600);
-		
-		//desenha a tela de gameplay
-		g.setColor(Color.black);
-		g.fillRect(25, 75, 851, 600);
-		
 	}
 }
+
+
